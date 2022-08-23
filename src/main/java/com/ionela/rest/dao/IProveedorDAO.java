cc04920ead4cbb7a4d7a2ce0499c772353a7ca67
vladasel <ionelavladasel2710@gmail.com>
2022-08-23 19:05:25 +0200

    fiz structure

--- src/main/java/com/ionela/rest/dao/IProveedorDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ionela.rest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ionela.rest.dto.Proveedor;
-
-public interface IProveedorDAO extends JpaRepository<Proveedor,String>{
-
-}
--- src/main/java/com/ionela/rest/dao/ISuministraDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ionela.rest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ionela.rest.dto.Suministra;
-
-public interface ISuministraDAO extends JpaRepository<Suministra,Long>{
-
-}
